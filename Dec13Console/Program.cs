a853cdf1b54d20cf4c1de8d6d9779bc5fc4e2ed4
HP <HP@DESKTOP-9CISLHK>
2023-12-16 22:06:52 +0530

    Add project files.

---
+++ Dec13Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec13Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats up");//class.method
+            Console.ReadLine();
+        }
+    }
+}
